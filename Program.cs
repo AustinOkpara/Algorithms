7304ba2b9edb1fe77f8a4f5a1ec00a1a7eef9f47
austinokpara <stinzy94@gmail.com>
2023-06-22 23:04:25 +0100

    Algo 6

--- Program.cs
+++ Program.cs
@@ -50,6 +50,16 @@ namespace Algos_Practice
 
             Console.WriteLine(string.Join(", ", number));
 
+            //Test for Algo 6
+            int[] numbers = { 0, 1, 0, 3, 12 };
+            ALGO6 soln = new ALGO6();
+            soln.MoveZeroes(nums);
+
+            foreach (int numm in nums)
+            {
+                Console.Write(numm + " ");
+            }
+
         }
     }
 
@@ -181,6 +191,30 @@ namespace Algos_Practice
             return;
         }
 
+    }
+
+    //ALGO6
+    public class ALGO6
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            while (count < nums.Length)
+            {
+                nums[count] = 0;
+                count++;
+            }
+
+        }
     }
 
 }
