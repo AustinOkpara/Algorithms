ce88131ff6318d0570121e4d6f30c06243fa6c1c
austinokpara <stinzy94@gmail.com>
2023-06-25 20:38:49 +0100

    Algo 10

--- Program.cs
+++ Program.cs
@@ -284,4 +284,38 @@ namespace Algos_Practice
             return string.Join(" ", words);
         }
     }
+
+    // ALGO 10
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+            this.val = val;
+            this.next = next;
+      }
+
+    }
+
+
+
+
+    public class ALGO10
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+
+        }
+    }
 }
