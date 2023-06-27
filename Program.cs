9cb9c0d12690448a74468e777da81e6c0d48f26f
austinokpara <stinzy94@gmail.com>
2023-06-27 20:01:53 +0100

    Algo 16

--- Program.cs
+++ Program.cs
@@ -510,4 +510,34 @@ namespace Algos_Practice
 
 
     }
+
+    //Algo 16
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+            if (t2 == null)
+                return t1;
+
+            TreeNode mergedNode = new TreeNode(t1.val + t2.val);
+            mergedNode.left = MergeTrees(t1.left, t2.left);
+            mergedNode.right = MergeTrees(t1.right, t2.right);
+
+            return mergedNode;
+        }
+    }
+
 }
