aba5fd2bc86869f46fc6735c831a2f0d5f1bac54
austinokpara <stinzy94@gmail.com>
2023-06-28 22:56:59 +0100

    Algo 18

--- Program.cs
+++ Program.cs
@@ -584,5 +584,55 @@ namespace Algos_Practice
         }
     }
 
+    //Algo 18
+
+    public class ALGO18
+    {
+        public int[][] UpdateMatrix(int[][] mat)
+        {
+            int rows = mat.Length;
+            int cols = mat[0].Length;
+
+            int[][] directions = { new int[] { -1, 0 }, new int[] { 1, 0 },
+            new int[] { 0, -1 }, new int[] { 0, 1 } };
+
+            Queue<int[]> queue = new Queue<int[]>();
+
+         
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (mat[i][j] == 0)
+                        queue.Enqueue(new int[] { i, j });
+                    else
+                        mat[i][j] = int.MaxValue;
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                int[] cell = queue.Dequeue();
+                int row = cell[0];
+                int col = cell[1];
+                foreach (int[] direction in directions)
+                {
+                    int newRow = row + direction[0];
+                    int newCol = col + direction[1];
+
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 &&
+                    newCol < cols && mat[row][col] + 1 < mat[newRow][newCol])
+                    {
+                        mat[newRow][newCol] = mat[row][col] + 1;
+                        queue.Enqueue(new int[] { newRow, newCol });
+                    }
+                }
+            }
+
+            return mat;
+
+        }
+    }
+
 
 }
