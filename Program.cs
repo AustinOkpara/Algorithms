223f2cd2c03d180183eb9756a548e0df515d4bfd
austinokpara <stinzy94@gmail.com>
2023-06-29 22:05:55 +0100

    Algo 21

--- Program.cs
+++ Program.cs
@@ -739,5 +739,31 @@ namespace Algos_Practice
         }
     }
 
+    //Algo 21
+
+    public class ALGO21
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+
 
 }
